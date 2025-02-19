26866501e31dce7096e0b2ae8a54e831b18bb1bc
Qexat <43090614+qexat@users.noreply.github.com>
2025-02-19 15:16:12 +0100

    improve ENV interface

--- lib/Env.ml
+++ lib/Env.ml
@@ -1,3 +1,5 @@
+open Util
+
 module type TOKENIZABLE_WITH_RELATION = sig
   include Ansifmt.Formatting.TOKENIZABLE
 
@@ -7,12 +9,22 @@ end
 module type ENV = sig
   type key
   type term
-  type t = (key * term) list
+  type t = private (key * term) list
 
   val empty : t
+  val keys : t -> key list
+  val terms : t -> term list
+  val rows : t -> (key * term list) list
+  val contains : key -> t -> bool
   val get : key -> t -> term option
-  val update_or_add : key -> term -> t -> t
-  val map : 'term2. (term -> 'term2) -> t -> (key * 'term2) list
+  val get_all : key -> t -> term list
+  val add : key -> term -> t -> t
+  val remove : key -> t -> t option
+  val remove_lenient : key -> t -> t
+  val update : key -> term -> t -> t option
+  val update_lenient : key -> term -> t -> t
+  val deduplicate : conflict_handler:(term list -> term) -> t -> t
+  val map_terms : 'term2. (term -> 'term2) -> t -> (key * 'term2) list
   val tokenize : t -> Ansifmt.Formatting.Tree.t
 end
 
@@ -23,16 +35,52 @@ module Make (Key : Ansifmt.Formatting.TOKENIZABLE) (Term : TOKENIZABLE_WITH_RELA
   type t = (key * term) list
 
   let empty : t = []
+  let keys : t -> key list = fun env -> env |> List.map fst |> List.deduplicate
+  let terms : t -> term list = List.map snd
+  let contains : key -> t -> bool = List.mem_assoc
   let get : key -> t -> term option = List.assoc_opt
 
-  let update_or_add : key -> term -> t -> t =
-    fun key term env ->
-    match get key env with
-    | None -> (key, term) :: env
-    | Some _ -> (key, term) :: List.remove_assoc key env
+  let get_all : key -> t -> term list =
+    fun key env -> env |> List.find_all (fun (key', _) -> compare key key' = 0) |> terms
   ;;
 
-  let map (type term2) (func : term -> term2) : t -> (key * term2) list = function
+  let rows : t -> (key * term list) list =
+    fun env -> List.map (fun key -> key, get_all key env) (keys env)
+  ;;
+
+  let add : key -> term -> t -> t = fun key term env -> (key, term) :: env
+
+  let remove_lenient : key -> t -> t =
+    let rec tailrec key env acc =
+      match env with
+      | [] -> acc
+      | (key', term') :: rest ->
+        tailrec key rest (if compare key key' = 0 then acc else add key' term' acc)
+    in
+    fun key env -> tailrec key env empty
+  ;;
+
+  let remove : key -> t -> t option =
+    fun key env ->
+    match contains key env with
+    | false -> None
+    | true -> Some (remove_lenient key env)
+  ;;
+
+  let update_lenient : key -> term -> t -> t =
+    fun key term env -> add key term (remove_lenient key env)
+  ;;
+
+  let update : key -> term -> t -> t option =
+    fun key term env -> remove key env |> Option.map (add key term)
+  ;;
+
+  let deduplicate : conflict_handler:(term list -> term) -> t -> t =
+    fun ~conflict_handler env ->
+    rows env |> List.map (fun (key, terms) -> key, conflict_handler terms)
+  ;;
+
+  let map_terms (type term2) (func : term -> term2) : t -> (key * term2) list = function
     | [] -> []
     | env -> List.map (fun (key, term) -> key, func term) env
   ;;
