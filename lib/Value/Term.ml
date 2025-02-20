2aefab6858ee856253895191a1d8688214e08af1
Qexat <43090614+qexat@users.noreply.github.com>
2025-02-20 10:20:28 +0100

    Rename `Mapping` from `Env` (`term` -> `value`) - Add functions `flip`, `iter` - Revamp `tokenize` to merge values of identical keys together - Apply renaming to its uses

--- lib/Value/Term.ml
+++ lib/Value/Term.ml
@@ -1,6 +1,6 @@
 open Util
 include Term_base
-module Env = Env.Make (Name) (Term_base)
+module Env = Mapping.Make (Name) (Term_base)
 
 let eval ~(env : Env.t) : t -> (t, Error.t) result = function
   | Var name -> Env.get name env |> Option.to_result ~none:(Error.Undefined_name name)
